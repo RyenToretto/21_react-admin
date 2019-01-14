77a69d3ed56ff1e6a5308cad6295a27f100ab929
RyenToretto <18273727925@163.com>
2019-01-14 13:14:52 +0800

    BrowserRouter, Switch, Route 路由 搭建好了

---
+++ src/App/App.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+import {BrowserRouter, Switch, Route} from "react-router-dom";
+
+import Login from "./Login/Login";
+import Admin from "./Admin/Admin";
+
+import "./css/App.css";
+
+export default class App extends Component {
+    render() {
+        return (
+            <BrowserRouter>
+                <Switch>
+                    <Route path="/login" component={Login} />
+                    <Route path="/admin" component={Admin} />
+                    <Route path="/" component={Login} />
+                </Switch>
+            </BrowserRouter>
+        );
+    }
+};
