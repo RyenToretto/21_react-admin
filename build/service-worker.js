0a74f2c2cb48ca1cf90ed4a82fa5b6ec72ce3280
RyenToretto <18273727925@163.com>
2019-01-14 19:15:28 +0800

    登录表单验证 - ant-design 的 UI 设计 - npm run build 问题 /static

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2c16fa25ec00e580957928cef1d0e057.js"
+  "./precache-manifest.d746315d63a1c7c9efe860239575ad50.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
