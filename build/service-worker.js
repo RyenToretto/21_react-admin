11ba84ab720935bad4074f6d61d46bac9816a3a7
RyenToretto <18273727925@163.com>
2019-01-16 09:30:59 +0800

    leftNav 菜单栏导航完成了-路由页面跳转

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.665fa16965fdcaa7ca3ae05dde9941e2.js"
+  "/build/precache-manifest.b0f8953d12bb1e90f4d210d503d7963f.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("./index.html", {
+workbox.routing.registerNavigationRoute("/build/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
