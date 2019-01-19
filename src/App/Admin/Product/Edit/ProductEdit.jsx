c8c10b294d38a4dab745d568dc364b05831a2b7e
RyenToretto <18273727925@163.com>
2019-01-19 22:56:21 +0800

    同步收集 product 的 input 数据搞定

--- src/App/Admin/Product/Edit/ProductEdit.jsx
+++ src/App/Admin/Product/Edit/ProductEdit.jsx
@@ -8,43 +8,105 @@ export default class ProductEdit extends Component {
     constructor(props){
         super(props);
         this.state = {
-            isAdd: true
+            product: {},
+            theTitle: "添加商品"
         }
     }
+    
+    input2Product = (e, type)=>{
+        const {product} = this.state;
+        product[type] = e.target.value;
+        this.setState({product});
+    };
+    
+    componentWillMount(){
+        const product = this.props.location.state;
+        let theTitle = "添加商品";
+        if(product.key){
+            theTitle = "编辑商品"
+        }
+        this.setState({
+            product,
+            theTitle
+        })
+    }
+    /****
+        key: "5c414dc66943f01eb45bab6b"
+        name: "吉普牛仔裤"
+        desc: "四季款"
+        pCategoryId: "5c3f11eec04a871c08734977"
+        categoryId: "5c401adfb8491a1b807fd9e3"
+        price: 99
+        imgs: ["image-1547783608353.jpg"]
+        detail: "<p>Handsome boy.</p>"
+        status: 1
+     ****/
     render(){
+        const {product, theTitle} = this.state;
+        let isAdd = true;
+        if(product.key){
+            isAdd = false;
+        }
+        
         return (
             <div className="product_edit">
                 <h3 className="product_nav">
                     <a href="javascript:" onClick={()=>{this.props.history.goBack()}}><Icon type="arrow-left"/></a>
-                    <span>{this.state.isAdd?"添加商品":"编辑商品信息"}</span>
+                    <span>{theTitle}</span>
                 </h3>
                 <div className="product_info_box">
                     <div>
                         <label>商品名称：
-                            <Input className="edit_product_name" type="text" defaultValue=""/>
+                            <Input
+                                className="edit_product_name"
+                                type="text"
+                                onChange={(e)=>this.input2Product(e, "name")}
+                                defaultValue={isAdd?"":product.name}
+                            />
                         </label>
                     </div>
                     <div>
                         <label>商品描述：
-                            <Input className="edit_product_desc" type="text" defaultValue=""/>
+                            <Input
+                                className="edit_product_desc"
+                                type="text"
+                                onChange={(e)=>this.input2Product(e, "desc")}
+                                defaultValue={isAdd?"":product.desc}
+                            />
                         </label>
                     </div>
                     <div className="padding_zero">
                         所属分类：
-                        <Select className="edit_product_category" defaultValue={2}>
-                            <Select.Option key={1} value={1}>1</Select.Option>
-                            <Select.Option key={2} value={2}>2</Select.Option>
-                            <Select.Option key={3} value={3}>3</Select.Option>
+                        <Select
+                            className="edit_product_category"
+                            defaultValue={
+                                isAdd?"未选择":
+                                    (product.pCategoryId==="0"?product.categoryId:
+                                        product.pCategoryId)
+                            }
+                        >
+                            <Select.Option key="0" value="0">一级品类</Select.Option>
+                            <Select.Option key="2" value="2">2</Select.Option>
+                            <Select.Option key="3" value="3">3</Select.Option>
                         </Select>
-                        <Select className="edit_product_category" defaultValue="c">
-                            <Select.Option key="a" value="a">a</Select.Option>
-                            <Select.Option key="b" value="b">b</Select.Option>
-                            <Select.Option key="c" value="c">c</Select.Option>
-                        </Select>
+                        {(isAdd || product.pCategoryId==="0")?null:(
+                            <Select
+                                className="edit_product_category"
+                                defaultValue={product.categoryId}
+                            >
+                                <Select.Option key="a" value="0">不可能的 一级品类</Select.Option>
+                                <Select.Option key="b" value="b">b</Select.Option>
+                                <Select.Option key="c" value="c">c</Select.Option>
+                            </Select>
+                        )}
                     </div>
                     <div>
                         <label className="edit_product_price">商品价格：
-                            <Input type="text" defaultValue="" addonAfter="元"/>
+                            <Input
+                                type="text"
+                                addonAfter="元"
+                                onChange={(e)=>this.input2Product(e, "price")}
+                                defaultValue={isAdd?"":product.price}/>
                         </label>
                     </div>
                     <div>
