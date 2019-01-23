a601a7119e04914daae5e09571312b43bbdc033a
RyenToretto <18273727925@163.com>
2019-01-23 23:11:11 +0800

    角色 + 权限管理 = 目录控制 ---- 搞定

--- src/App/Admin/LeftNav/LeftNav.jsx
+++ src/App/Admin/LeftNav/LeftNav.jsx
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import {withRouter, NavLink} from "react-router-dom";
 import {Menu, Icon} from "antd";
 import {menuList,getCurKey} from "./config/menuConfig";
+import myLocalStorage from "../../../tools/storeLocalStorage";
 
 import "./css/LeftNav.css";
 import theLogo from "../../Login/img/logo.png";
@@ -10,37 +11,54 @@ import theLogo from "../../Login/img/logo.png";
 class LeftNav extends Component {
     
     menuItemArr = (menuConfig)=>{
+        const curUser = myLocalStorage.local("user_key");
+        
+        const isAdmin = curUser.username === "admin";
+        const curPaths = new Set(curUser.role.menus);
+        
         return menuConfig.reduce((pre,item)=>{
             let menuItem = null;
             if(item.children){
-                menuItem = (
-                    <Menu.SubMenu
-                        key={item.key}
-                        title={
-                            <span>
+                let shouldContinue = false;
+                for(let i=0; i<item.children.length; i++){
+                    shouldContinue = curPaths.has(item.children[i].key);
+                    if(shouldContinue){
+                        break;
+                    }
+                }
+                if(item.key==="/home" || isAdmin || shouldContinue){
+                    menuItem = (
+                        <Menu.SubMenu
+                            key={item.key}
+                            title={
+                                <span>
                                 <Icon type={item.icon}/>
                                 <span>
                                     {item.title}
                                 </span>
                             </span>
-                        }
-                    >
-                        {
-                            this.menuItemArr(item.children)
-                        }
-                    </Menu.SubMenu>
-                )
+                            }
+                        >
+                            {
+                                this.menuItemArr(item.children)
+                            }
+                        </Menu.SubMenu>
+                    )
+                }
             }else{
-                menuItem = (
-                    <Menu.Item key={item.key}>
-                        <NavLink to={item.key}>
-                            <Icon type={item.icon}/>
-                            {item.title}
-                        </NavLink>
-                    </Menu.Item>
-                )
+                if(item.key==="/home" || isAdmin || curPaths.has(item.key)){
+                    menuItem = (
+                        <Menu.Item key={item.key}>
+                            <NavLink to={item.key}>
+                                <Icon type={item.icon}/>
+                                {item.title}
+                            </NavLink>
+                        </Menu.Item>
+                    )
+                }
             }
-            pre.push(menuItem);
+            
+            menuItem && pre.push(menuItem);
             return pre;
         }, []);
     };
