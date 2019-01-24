6bd7f6f6d76eb95635dd2f474d87aa996338631e
RyenToretto <18273727925@163.com>
2019-01-24 10:47:27 +0800

    整个项目都搞定了 - over

--- src/App/Admin/Bar/Bar.jsx
+++ src/App/Admin/Bar/Bar.jsx
@@ -1,11 +1,43 @@
 import React, { Component } from 'react';
 
+import {Card} from "antd";
+import ReactEcharts from "echarts-for-react";
+
 import "./css/Bar.css";
 
 export default class Bar extends Component {
+    getOption = ()=>{
+        return {
+            title: {
+                text: 'ECharts 入门示例'
+            },
+            tooltip: {},
+            legend: {
+                data:['销量']
+            },
+            xAxis: {
+                data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+            },
+            yAxis: {},
+            series: [{
+                name: '销量',
+                type: 'bar',
+                data: [5, 20, 36, 10, 10, 20]
+            }]
+        }
+    };
+    
     render(){
         return (
-            <h2>Bar</h2>
+            <Card
+                className="bar_card"
+                bordered={false}
+                title="柱状图"
+            >
+                <ReactEcharts
+                    option = {this.getOption()}
+                />
+            </Card>
         )
     }
 }
--- src/App/Admin/Line/Line.jsx
+++ src/App/Admin/Line/Line.jsx
@@ -1,11 +1,53 @@
 import React, { Component } from 'react';
 
+import {Card} from "antd";
+import ReactEcharts from "echarts-for-react";
 import "./css/Line.css";
 
 export default class Line extends Component {
+    getOption = ()=>{
+        return {
+            xAxis: {
+                type: 'category',
+                data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+            },
+            yAxis: {
+                type: 'value'
+            },
+            series: [{
+                data: [120, 200, 150, 80, 70, 110, 130],
+                type: 'line',
+                symbol: 'triangle',
+                symbolSize: 20,
+                lineStyle: {
+                    normal: {
+                        color: 'green',
+                        width: 4,
+                        type: 'dashed'
+                    }
+                },
+                itemStyle: {
+                    normal: {
+                        borderWidth: 3,
+                        borderColor: 'yellow',
+                        color: 'blue'
+                    }
+                }
+            }]
+        }
+    };
+    
     render(){
         return (
-            <h2>Line</h2>
+            <Card
+                className="line_card"
+                bordered={false}
+                title="柱状图"
+            >
+                <ReactEcharts
+                    option = {this.getOption()}
+                />
+            </Card>
         )
     }
 }
